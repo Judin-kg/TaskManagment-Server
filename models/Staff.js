373b8525fe740c1bb83b25521709fdf01132c792
vishnuaxobis <vishnu@axobis.com>
2025-09-22 12:07:24 +0530

    new

--- models/Staff.js
+++ models/Staff.js
@@ -18,12 +18,12 @@ const staffSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-staffSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// staffSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
 
 // Compare passwords
 staffSchema.methods.matchPassword = async function (enteredPassword) {
