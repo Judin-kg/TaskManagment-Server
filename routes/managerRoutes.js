1701f84c6f274c3d3dccdcc71572d8a59b522bf2
vishnuaxobis <vishnu@axobis.com>
2025-09-26 09:40:27 +0530

    route change

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getManagers, createManager,getManagersByDepartment,loginManager } = require("../controllers/managerController");
+const { getManagers, createManager,getManagersByDepartment,loginManager,deleteManager} = require("../controllers/managerController");
 
 // GET all managers
 router.get("/", getManagers);
@@ -8,6 +8,7 @@ router.get("/", getManagers);
 // POST create manager
 router.post("/", createManager);
 router.post("/login", loginManager);
+router.delete("/:id", deleteManager);
 // Get Managers by Department (optional filter)
 router.get("/department/:departmentId",getManagersByDepartment);
 
