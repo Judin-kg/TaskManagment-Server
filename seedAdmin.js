2d20cf029a2f52a2cef0b98055fcefec7cc35b90
vishnuaxobis <vishnu@axobis.com>
2025-09-30 18:25:11 +0530

    update

--- seedAdmin.js
+++ seedAdmin.js
@@ -1,14 +1,14 @@
-const Staff = require("./models/User");
+const Auth = require("./models/Admin");
 
 async function seedAdmin() {
   try {
-    const exists = await Staff.findOne({ email: process.env.ADMIN_EMAIL || "rj@example.com" });
+    const exists = await Auth.findOne({ email: process.env.ADMIN_EMAIL || "rj@example.com" });
     if (exists) {
       console.log("⚠️ Admin already exists, skipping seeding");
       return;
     }
 
-    const admin = new Staff({
+    const admin = new Auth({
       username: "superadmin",  // 👈 required field
       email: process.env.ADMIN_EMAIL || "rj@example.com",
       password: process.env.ADMIN_PASS || "securepassword123",
